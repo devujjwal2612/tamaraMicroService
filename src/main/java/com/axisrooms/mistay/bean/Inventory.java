cb7864f0094bcd4dbb03cf40bccb299adcb6d866
Sandeep <sandeep.md@axisrooms.com>
2018-08-02 14:26:09 +0530

    unwanted files removed

--- src/main/java/com/axisrooms/mistay/bean/Inventory.java
+++ src/main/java/com/axisrooms/mistay/bean/Inventory.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
+
 @Data
 public class Inventory {
     @JsonFormat(pattern = Constants.DATE_PATTERN)
