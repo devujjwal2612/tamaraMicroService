bce69f76254d6631337eb87bf9f3ee41d126fe65
Sandeep <sandeep.md@axisrooms.com>
2018-10-29 18:10:00 +0530

    added with jsonproperty annotation

--- src/main/java/com/axisrooms/mistay/generated/updateInventory/Data.java
+++ src/main/java/com/axisrooms/mistay/generated/updateInventory/Data.java
@@ -3,6 +3,7 @@ package com.axisrooms.mistay.generated.updateInventory;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
@@ -10,7 +11,10 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Data {
+    @JsonProperty("inventory")
     private List<Inventory> inventory;
+    @JsonProperty("propertyId")
     private String propertyId;
+    @JsonProperty("roomId")
     private String roomId;
 }
--- src/main/java/com/axisrooms/mistay/generated/updateInventory/Inventory.java
+++ src/main/java/com/axisrooms/mistay/generated/updateInventory/Inventory.java
@@ -5,6 +5,7 @@ import com.axisrooms.mistay.util.Constants;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
 
@@ -12,9 +13,12 @@ import java.time.LocalDate;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Inventory {
+    @JsonProperty("endDate")
     @JsonFormat(pattern = Constants.DATE_PATTERN)
     private LocalDate endDate;
+    @JsonProperty("free")
     private Integer      free;
     @JsonFormat(pattern = Constants.DATE_PATTERN)
+    @JsonProperty("startDate")
     private LocalDate    startDate;
 }
